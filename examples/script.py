9516a13fa2ef8fe981f1c2cf15c846178e39c6a8
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-09-09 13:55:26 +0300

    Add solution 2.4.2

--- examples/script.py
+++ examples/script.py
@@ -1,3 +1,11 @@
 # This is our first Python script
 name = "world"
 print("hello " + name + "!")  # a simple print statement
+
+multiline_str = """x
+Out[41]: 3.1456456
+z
+Out[42]: 100
+z == 100"""
+
+print(multiline_str)

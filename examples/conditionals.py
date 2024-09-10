d188b75f2a5bbdc6b9465ea8b91319440168c398
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-09-10 10:35:23 +0300

    Add solution 3.1.6

--- examples/conditionals.py
+++ examples/conditionals.py
@@ -5,3 +5,14 @@ if is_cat is True:
     print(f"{name} is a cat.")
 
 print("Bye from cat world!")
+
+x, y = 10, 20
+
+if x > 5:
+    if y > 15:
+        print("Both x and y are greater than their respective thresholds.")
+        print("Still under if y > 15")
+    else:
+        print("x is greater than 5, but y is not greater than 15.")
+else:
+    print("x is not greater than 5.")
